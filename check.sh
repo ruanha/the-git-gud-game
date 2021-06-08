a8b335834dedd4e6dab777ac12373d404c5606d9
rune <rune_hartvig@hotmail.com>
2021-06-08 21:38:49 +0200

    init level state if empty

--- check.sh
+++ check.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 echo "progressing game..."
 
+#if level state files don't exist. initiate them
+if [[ ! -f ".gamefiles/LVL" ]]; then
+	echo 0 > ".gamefiles/LVL"
+	chmod "a+w" ".gamefiles/LVL"
+fi
+if [[ ! -f ".gamefiles/SUBLVL" ]]; then
+	echo 0 > ".gamefiles/SUBLVL" 
+	chmod "a+w" ".gamefiles/SUBLVL"
+fi
+
+# Load the level state
 LVL=$( < ".gamefiles/LVL" )
 SUBLVL=$( < ".gamefiles/SUBLVL" )
 
