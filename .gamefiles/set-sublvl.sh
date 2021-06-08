c195833418a043370425f1fb0ce0aa617515fdb6
rune <rune_hartvig@hotmail.com>
2021-06-08 22:14:33 +0200

    write to inbox

--- .gamefiles/set-sublvl.sh
+++ .gamefiles/set-sublvl.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 THIS_PATH=$( dirname "${BASH_SOURCE[0]}" )
 echo $1 > "$THIS_PATH/SUBLVL"
+
+LVL=$( < "$THIS_PATH/LVL" )
+SUBLVL=$( < "$THIS_PATH/SUBLVL" )
+
+EMAIL=$( < "$THIS_PATH/solutions/$LVL/$SUBLVL/email" )
+echo "youve for mail $EMAIL"
+echo $EMAIL > "email.inbox"
