b803c62d847e2e252965fe7a35fd01b131b93ec3
rune <rune_hartvig@hotmail.com>
2021-06-14 09:45:41 +0200

    source app name from a conf file

--- .gamefiles/solutions/0/solution.sh
+++ .gamefiles/solutions/0/solution.sh
@@ -3,8 +3,12 @@
 THIS_PATH=$( dirname "${BASH_SOURCE[0]}" )
 SUBLVL=$1
 
-PATH_1="mock-app-0/" 
-PATH_2="$THIS_PATH/$SUBLVL/mock-app-0/"
+# load level config: APP_NAME
+source "conf.d"
+echo "app name: $APP_NAME"
+
+PATH_1="$APP_NAME/" 
+PATH_2="$THIS_PATH/$SUBLVL/$APP_NAME/"
 
 if diff -rq -wB -x '.git' $PATH_1 $PATH_2
 then
