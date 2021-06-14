a45c5ce65e4de2f28310812a5a8c05b9bf8687c4
rune <rune_hartvig@hotmail.com>
2021-06-14 21:28:32 +0200

    removes the local repo and replace the remote

--- reset.sh
+++ reset.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# get APP_NAME from conf.d
+LVL=$( < ".gamefiles/LVL" )
+source ".gamefiles/solutions/$LVL/conf.d"
+
+# remove local app and the git-gud.com remote
+rm -rf "$APP_NAME"
+rm -rf "git-gud.com/mock-org-0/$APP_NAME.git"
+
+# set sublvl back to zero. This includes setting the email
+# message and replacing the repo
+.gamefiles/set-lvl.sh $LVL
 .gamefiles/set-sublvl.sh 0
 
 
