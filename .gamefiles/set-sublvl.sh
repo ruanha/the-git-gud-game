0aaa34f7fef7312fd288625f62c7c4d2389e1624
rune <rune_hartvig@hotmail.com>
2021-07-04 21:18:28 +0200

    replace new email with soft link

--- .gamefiles/set-sublvl.sh
+++ .gamefiles/set-sublvl.sh
@@ -5,9 +5,8 @@ echo $1 > "$THIS_PATH/SUBLVL"
 LVL=$( < "$THIS_PATH/LVL" )
 SUBLVL=$( < "$THIS_PATH/SUBLVL" )
 
-EMAIL=$( < "$THIS_PATH/solutions/$LVL/$SUBLVL/email" )
-cat "$THIS_PATH/solutions/$LVL/$SUBLVL/email" > "email.inbox"
+ln -sf "$THIS_PATH/solutions/$LVL/$SUBLVL/email"
 
 echo "############ EMAIL INBOX ############"
-cat "$THIS_PATH/solutions/$LVL/$SUBLVL/email"
+cat "email"
 echo "#####################################"
